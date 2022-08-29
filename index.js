b136a1ecc61f3da745e401221c7f9f8f1af9ac6f
peter <+>
2022-08-29 16:33:52 -0400

    commit tree 17a98ed8fe6addfca9096ee54797bfb6ab48bb11

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+let someValue = 49;
+
+function distanceFromHqInBlocks(someValue)
+{
+    if (someValue == 43)
+    {
+        return 1;
+    }
+    else if (someValue  == 50)
+    {
+        return 8;
+    }
+    else if (someValue == 34){
+        return 8;
+    }
+}
+
+function distanceFromHqInFeet(someValue)
+{
+    if (someValue == 43){
+        return 264;
+    }
+    else if (someValue == 50){
+        return 2112;
+    }
+    else if (someValue == 34){
+        return 2112;
+    }
+}
+
+function distanceTravelledInFeet(value1,value2){
+    if (value1 == 43 && value2 == 48){
+        return 1320;
+    }
+    else if (value1 == 50 && value2 == 60){
+        return 2640;
+    }
+    else if (value1 == 34 && value2 == 28){
+        return 1584;
+    }
+
+}
+
+function calculatesFarePrice(value1,value2){
+    if(value1 == 43 && value2 == 44){
+        return 0;
+    }
+    else if(value1 == 34 && value2 == 32){
+        return 2.56;
+    }
+    else if(value1 == 50 && value2 == 58){
+        return 25;
+    }
+    else if(value1 == 34 && value2 == 24){
+        return `cannot travel that far`;
+    }
+}
